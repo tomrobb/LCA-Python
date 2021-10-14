f28c9bdfd5bbfdc6a731e5bf9f02cddf2fcb684f
Tom <robertto@tcd.ie>
2021-10-14 23:35:10 +0100

    Updated test code

--- LCA.py
+++ LCA.py
@@ -10,45 +10,60 @@ class Node:
 # function that returns the pointer to the LCA of two given nodes
 # assumes n1 and n2 are present in the tree
 
-def findLCA(root, n1, n2):
-    if root is None:
-        return None
+class BinaryTree:
 
-    # If either n1 or n2 matches with root's key, report
-    #  the presence by returning root (Note that if a key is
-    #  ancestor of other, then the ancestor key becomes LCA
-    if root.key == n1 or root.key == n2:
-        return root
+    def __init__(self):
+        self.root = None
 
-    # Look for keys in left and right subtrees
-    left_lca = findLCA(root.left, n1, n2)
-    right_lca = findLCA(root.right, n1, n2)
+    def findNode(self, key):
+        return self.findNode2(self.root, key)
 
-    # If both of the above calls return Non-NULL, then one key
-    # is present in once subtree and other is present in other,
-    # So this node is the LCA
-    if left_lca and right_lca:
-        return root
+    def findNode2(self, node, key):
+        # when node = null
+        if node is None or node.key == key:
+            return node
 
-    # Otherwise check if left subtree or right subtree is LCA
-    return left_lca if left_lca is not None else right_lca
+        # when key > node
+        if key > node.key:
+            return self.findNode2(node.right, key)
+
+        # when key < node
+        return self.findNode2(node.left, key)
+
+    def addNode(self, key):
+        self.root = self.addNode2(self.root, key)
 
 
-# Testing with the following binary tree:
-#                              1
-#                         /         \
-#                        2            3
-#                    /       \     /     \
-#                  4         5    6        7
+    def addNode2(self, node, key):
+        if node is None:
+            node = Node(key)
+            return node
 
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-root.right.left = Node(6)
-root.right.right = Node(7)
-print("The LCA of 6 and 7 = ", findLCA(root, 6, 7).key)  # 3
-print("The LCA of 4 and 5 = ", findLCA(root, 4, 5).key)  # 2
-print("The LCA of 2 and 6 = ", findLCA(root, 2, 6).key)  # 1
-print("The LCA of 3 and 7 = ", findLCA(root, 3, 7).key)  # 3
+        if key < node.key:
+            node.left = self.addNode2(node.left, key)
+        else:
+            node.right = self.addNode2(node.right, key)
+
+        return node
+
+
+    def findLCA(self, n1, n2):
+        return self.findLCA2(self.root, n1, n2)
+
+    def findLCA2(self, node, n1, n2):
+        # Return None if tree is empty
+        if node is None:
+            return None
+
+        # If both n1 and n2 are smaller than root
+        if node.key > n1 and node.key > n2:
+            return self.findLCA2(node.left, n1, n2)
+
+        # If both n1 and n2 are greater than root
+        if node.key < n1 and node.key < n2:
+            return self.findLCA2(node.right, n1, n2)
+
+        # Else return node
+        return node
+
+
--- test_LCA.py
+++ test_LCA.py
@@ -1,26 +1,92 @@
 from unittest import TestCase
+
+import LCA
 from LCA import *
 
 
 class Test(TestCase):
-    def test_find_lca(self):
+
+    def testNode(self):
+        node = LCA.Node(1)
+
+        self.assertEqual(1, node.key)
+
+    def testBinaryTree(self):
+        tree = LCA.BinaryTree()
+
+        self.assertEqual(None, tree.root)
+
+    def testFindNodeKeyNull(self):
+        tree = LCA.BinaryTree()
+
+        self.assertEqual(None, tree.findNode(10))
+
+    def testFindNodeEqualRoot(self):
+        tree = LCA.BinaryTree()
+        tree.addNode(10)
+
+        self.assertEqual(10, tree.findNode(10).key)
+
+    def testFindNodeBiggerRoot(self):
+        tree = LCA.BinaryTree()
+        tree.addNode(10)
+        tree.addNode(20)
+
+        self.assertEqual(20, tree.findNode(20).key)
+
+    def testFindNodeSmallerRoot(self):
+        tree = LCA.BinaryTree()
+        tree.addNode(20)
+        tree.addNode(10)
+
+        self.assertEqual(10, tree.findNode(10).key)
+
+    def testLeft(self):
+        tree = LCA.BinaryTree()
+        tree.addNode(10)
+        tree.addNode(5)
+
+        self.assertEqual(tree.findNode(5), tree.findNode(10).left)
+
+    def testRight(self):
+        tree = LCA.BinaryTree()
+        tree.addNode(10)
+        tree.addNode(15)
+
+        self.assertEqual(tree.findNode(15), tree.findNode(10).right)
+
+    def testLCANull(self):
+        tree = LCA.BinaryTree()
+        self.assertEqual(None, tree.findLCA(10,20))
+
+    def testLCASimple(self):
+        tree = LCA.BinaryTree()
+        tree.addNode(20)
+        tree.addNode(10)
+        tree.addNode(30)
+
+        self.assertEqual(tree.findNode(20), tree.findLCA(10,30))
+
+    def testLCA(self):
         # Testing with the following binary tree:
-        #                              1
+        #                              35
         #                         /         \
-        #                        2            3
+        #                        20           50
         #                    /       \     /     \
-        #                  4         5    6        7
+        #                  10         30   40     70
+        tree = LCA.BinaryTree()
+        tree.addNode(35)
+        tree.addNode(20)
+        tree.addNode(50)
+        tree.addNode(10)
+        tree.addNode(30)
+        tree.addNode(40)
+        tree.addNode(70)
+        self.assertEqual(tree.findNode(20), tree.findLCA(10, 30))
+        self.assertEqual(tree.findNode(50), tree.findLCA(40, 70))
+        self.assertEqual(tree.findNode(20), tree.findLCA(10, 20))
+        self.assertEqual(tree.findNode(50), tree.findLCA(50, 70))
+        self.assertEqual(tree.findNode(35), tree.findLCA(10, 70))
 
-        root = Node(1)
-        root.left = Node(2)
-        root.right = Node(3)
-        root.left.left = Node(4)
-        root.left.right = Node(5)
-        root.right.left = Node(6)
-        root.right.right = Node(7)
 
-        self.assertEqual(3, findLCA(root, 6, 7).key)
-        self.assertEqual(2, findLCA(root, 4, 5).key)
-        self.assertEqual(1, findLCA(root, 2, 6).key)
-        self.assertEqual(3, findLCA(root, 3, 7).key)
 
